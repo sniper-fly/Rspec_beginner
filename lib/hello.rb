05043eaf45d4f7fd0a16ba32a8180d988fefa1f7
RyoNakai <ryo.nakai@super-studio.jp>
2021-10-27 12:03:35 +0900

    describe, it,の使い方マスター

--- lib/hello.rb
+++ lib/hello.rb
@@ -3,3 +3,17 @@ class Hello
     "hello"
   end
 end
+
+class User
+  def initialize(name:, age:)
+    @name = name
+    @age = age
+  end
+  def greet
+    if @age <= 12
+      "ぼくは#{@name}だよ。"
+    else
+      "僕は#{@name}です。"
+    end
+  end
+end
--- spec/hello_spec.rb
+++ spec/hello_spec.rb
@@ -5,3 +5,30 @@ RSpec.describe Hello do
     expect(Hello.new.message).to eq "hello"
   end
 end
+
+RSpec.describe '四則演算' do
+  it '1 + 1 は 2 になること' do
+    expect(1 + 1).to eq 2
+  end
+  it '10 - 1 == 9 ??' do
+    expect(10 - 1).to eq 9
+  end
+end
+
+RSpec.describe User do
+  describe '#greet' do
+    it 'if under age 12, put hiragana' do
+      user = User.new(name: 'taro', age:12)
+      expect(user.greet).to eq 'ぼくはtaroだよ。'
+    end
+    it 'if age is more than 13, put kanji' do
+      user = User.new(name: 'taro', age: 13)
+      expect(user.greet).to eq "僕はtaroです。"
+    end
+  end
+
+end
+
+#describeはテスト対象をグループ化する
+#contextで条件や状況を明示する
+#itでテストする確認項目を表す
